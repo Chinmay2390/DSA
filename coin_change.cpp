351edfc013aac9701828f13922d2a02fb27ba170
Chinmay2390 <chinmaykamlaskar@gmail.com>
2025-01-26 10:22:54 +0530

    Add C++ DP solution for Coin Change

---
+++ coin_change.cpp
@@ -0,0 +1,24 @@
+// coin_change.cpp - Minimum number of coins to make amount
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+
+    for (int coin : coins) {
+        for (int i = coin; i <= amount; ++i) {
+            dp[i] = min(dp[i], dp[i - coin] + 1);
+        }
+    }
+
+    return dp[amount] > amount ? -1 : dp[amount];
+}
+
+int main() {
+    vector<int> coins = {1, 2, 5};
+    int amount = 11;
+    cout << "Min coins needed: " << coinChange(coins, amount) << endl;
+    return 0;
+}
